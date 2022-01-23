d9e09b2fc10993e9e4774865ee860ee0172a4131
Vishal541 <72211183+Vishal541@users.noreply.github.com>
2022-01-23 23:01:26 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+edWindow("Color detectors")
+# cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180,setValues)
+# cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255,setValues)
+# cv2.createTrackbar("Upper Value", "Color detectors", 255, 255,setValues)
+# cv2.createTrackbar("Lower Hue", "Color detectors", 4, 180,setValues)
+# cv2.createTrackbar("Lower Saturation", "Color detectors", 172, 255,setValues)
+# cv2.createTrackbar("Lower Value", "C
